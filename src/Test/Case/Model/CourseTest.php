40aeaa36a89e7c1ad5de611ecf9b909c59e3ce3e
Elmo Neto <analistaelmo@gmail.com>
2025-04-20 19:25:35 -0300

    Teste e Fixture básicos - O tetste, foi feito para ver se autoload funcionou. E fixture, veio junto com o bake da Model. Lembrando que o bake de model funcionar conforme a tabela do banco existe.

---
+++ src/Test/Case/Model/CourseTest.php
@@ -0,0 +1,45 @@
+<?php
+App::uses('Course', 'Model');
+
+/**
+ * Course Test Case
+ *
+ */
+class CourseTest extends CakeTestCase {
+
+/**
+ * Fixtures
+ *
+ * @var array
+ */
+	public $fixtures = array(
+		'app.course'
+	);
+
+/**
+ * setUp method
+ *
+ * @return void
+ */
+	public function setUp() {
+		parent::setUp();
+		$this->Course = ClassRegistry::init('Course');
+	}
+
+/**
+ * tearDown method
+ *
+ * @return void
+ */
+	public function tearDown() {
+		unset($this->Course);
+
+		parent::tearDown();
+	}
+
+	function testSum() {
+		$res = $this->Course->sum(1, 2);
+		$this->assertThat(3, $this->equalTo($res));
+	}
+
+}
---
+++ src/Test/Fixture/CourseFixture.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * CourseFixture
+ *
+ */
+class CourseFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'fullname' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8mb4_general_ci', 'charset' => 'utf8mb4'),
+		'shortname' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'utf8mb4_general_ci', 'charset' => 'utf8mb4'),
+		'hash' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 64, 'key' => 'unique', 'collate' => 'utf8mb4_general_ci', 'charset' => 'utf8mb4'),
+		'type' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 50, 'collate' => 'utf8mb4_general_ci', 'charset' => 'utf8mb4'),
+		'created' => array('type' => 'datetime', 'null' => true),
+		'modified' => array('type' => 'datetime', 'null' => true),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'hash' => array('column' => 'hash', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8mb4', 'collate' => 'utf8mb4_general_ci', 'engine' => 'InnoDB')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'fullname' => 'Lorem ipsum dolor sit amet',
+			'shortname' => 'Lorem ipsum dolor sit amet',
+			'hash' => 'Lorem ipsum dolor sit amet',
+			'type' => 'Lorem ipsum dolor sit amet',
+			'created' => '2025-04-20 01:04:52',
+			'modified' => '2025-04-20 01:04:52'
+		),
+	);
+
+}
